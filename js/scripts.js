d9e403eed1b5af9d1393ca84ba6157400ca7d9d6
Camille Delesalle <cdelesalle@MacBook-Air-de-Camille.local>
2015-12-31 02:09:43 +0100

    fix parralax + add audio play

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,57 @@ $(document).ready(function(){/* smooth scrolling for scroll to top */
 $('.scroll-down').click(function(){
   $('body,html').animate({scrollTop:$('#sec1').offset().top-50}, 1000);
 })
+
+/* artist audio hover*/
+$('.background-artist').find('.audio').hide();
+
+$('.background-artist').hover(function() {
+        $(this).find('.infos').hide();
+        $(this).find('.audio').show();
+    }, function() {
+        $(this).find('.audio').hide();
+        $(this).find('.infos').show();
+})
+
+/* artist music */
+var audio1 = document.createElement('audio');
+var audio2 = document.createElement('audio');
+var audio3 = document.createElement('audio');
+var audio4 = document.createElement('audio');
+
+audio1.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+audio2.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+audio3.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+audio4.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+
+audio1.addEventListener("load", function() {audio1.play();}, true);
+audio2.addEventListener("load", function() {audio2.play();}, true);
+audio3.addEventListener("load", function() {audio3.play();}, true);
+audio4.addEventListener("load", function() {audio4.play();}, true);
+
+$('.audio').find('.pause').hide();
+
+$('.play').click(function() {
+    var id = $(this).attr("audio-file");
+    eval("audio" + id + ".play()");
+    $('#artist-' + id + ' .play').hide();
+    $('#artist-' + id + ' .pause').show();
+});
+
+$('.pause').click(function() {
+    var id = $(this).attr("audio-file");
+    eval("audio" + id + ".pause()");
+    $('#artist-' + id + ' .pause').hide();
+    $('#artist-' + id + ' .play').show();
+});
+
 /* highlight the top nav as scrolling occurs */
 $('body').scrollspy({ target: '#navbar' })
+
+$('#sec2').parallax("center", 1500, 0.5, true);
+
+
 });
+
+
+
