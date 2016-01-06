7916a84a2768699d23e0102fa1c42313e5c10c4a
hugooh <hugo.delesalle@gmail.com>
2016-01-06 23:28:47 +0100

    song on artisists mosaic. finished website

--- js/scripts.js
+++ js/scripts.js
@@ -30,16 +30,41 @@ $(document).ready(function(){
 	var audio2 = document.createElement('audio');
 	var audio3 = document.createElement('audio');
 	var audio4 = document.createElement('audio');
+    var audio5 = document.createElement('audio');
+    var audio6 = document.createElement('audio');
+    var audio7 = document.createElement('audio');
+    var audio8 = document.createElement('audio');
+    var audio9 = document.createElement('audio');
+	var audio10 = document.createElement('audio');
+	var audio11 = document.createElement('audio');
+	var audio12 = document.createElement('audio');
 
-	audio1.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+	audio1.setAttribute('src', 'audio/Arve Henriksen - Ascent.mp3');
 	audio2.setAttribute('src', 'audio/GlimpseOfLight.mp3');
-	audio3.setAttribute('src', 'audio/GlimpseOfLight.mp3');
-	audio4.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+	audio3.setAttribute('src', 'audio/Miss Tati – Shakedown.mp3');
+	audio4.setAttribute('src', 'audio/from Slow Eastbound Train by Daniel Herskedal.mp3');
+    audio5.setAttribute('src', 'audio/Snorre Bjerck, Drumsolo Live.mp3'); 
+    audio6.setAttribute('src', 'audio/Mamadou Diabaté dit DG - 2015.mp3'); 
+    audio7.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+    audio8.setAttribute('src', 'audio/Shan Qi- Helge A. Norbakken, Moods.mp3');
+    audio9.setAttribute('src', 'audio/Martin Halla  Illuminate The Sky.mp3');
+	audio10.setAttribute('src', 'audio/Trio Mediaeval Nomemus.mp3');
+	audio11.setAttribute('src', 'audio/GlimpseOfLight.mp3');
+	audio12.setAttribute('src', 'audio/Ivar Kolve trio w- symph.orchestra.mp3');
 
 	audio1.addEventListener("load", function() {audio1.play();}, true);
 	audio2.addEventListener("load", function() {audio2.play();}, true);
 	audio3.addEventListener("load", function() {audio3.play();}, true);
 	audio4.addEventListener("load", function() {audio4.play();}, true);
+    audio5.addEventListener("load", function() {audio5.play();}, true);
+    audio6.addEventListener("load", function() {audio6.play();}, true);
+    audio7.addEventListener("load", function() {audio7.play();}, true);
+    audio8.addEventListener("load", function() {audio8.play();}, true);
+    audio9.addEventListener("load", function() {audio9.play();}, true);
+	audio10.addEventListener("load", function() {audio10.play();}, true);
+	audio11.addEventListener("load", function() {audio11.play();}, true);
+	audio12.addEventListener("load", function() {audio12.play();}, true);
+
 
 	$('.audio').find('.pause').hide();
 
